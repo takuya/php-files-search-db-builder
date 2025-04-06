9203cb82a6aa092b44fbce4e526a78feae331bb3
takuya <55338+takuya@users.noreply.github.com>
2025-04-07 04:08:20 +0900

    sample

---
+++ sample/build.php
@@ -0,0 +1,13 @@
+<?php
+
+use Takuya\SearchFiles\FindDbBuilder;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$db = './sample.db';
+file_exists($db) && unlink($db);
+$dir = $argv[1] ?? '/etc';
+$builder = new FindDbBuilder( "sqlite:{$db}", $dir );
+$builder->findSize('+10M');
+$builder->locates_build();
+
---
+++ sample/search.php
@@ -0,0 +1,11 @@
+<?php
+
+use Takuya\Utils\PdoTable\PdoTableRepository;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$db = './sample.db';
+$table = new PdoTableRepository(new PDO("sqlite:{$db}"),'locates_fts');
+$ret = $table->select('filename','match',$argv[1]??'ssh');
+dd($ret);
+
